cc17dd67cade3e4e9a2c4c685b70b0070092e1a7
lechengwang <sebaoyu@gmail.com>
2020-09-14 01:43:06 +0000

    Update file extension to .cpp

--- makefile
+++ makefile
@@ -14,7 +14,6 @@ LDFLAGS=-lm -L$(MYDIR)sprng/lib -llcg -static $(GSL_LDFLAG)
 #-------------------------------------------------------------------------
 
 CC=g++ 
-FC=gfortran
 
 #-------------------------------------------------------------------------
 # objects for QMC
@@ -26,29 +25,29 @@ mch2OBJS=mc_clsmc.o mc_estim.o mc_input.o mc_setup.o mc_poten.o mc_randg.o mc_ut
 mch2:			mc_main.o $(mch2OBJS) 
 			$(CC) $(options) -o $@  mc_main.o $(mch2OBJS) $(LDFLAGS) 
 
-mc_main.o:		mc_main.cc mc_confg.h mc_input.h mc_setup.h mc_randg.h mc_utils.h mc_clsmc.h mc_estim.h 
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_main.o:		mc_main.cpp mc_confg.h mc_input.h mc_setup.h mc_randg.h mc_utils.h mc_clsmc.h mc_estim.h 
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
  
-mc_input.o:        	mc_input.cc mc_input.h	mc_confg.h mc_setup.h mc_utils.h 
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_input.o:        	mc_input.cpp mc_input.h	mc_confg.h mc_setup.h mc_utils.h 
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
-mc_setup.o:        	mc_setup.cc mc_setup.h mc_confg.h mc_utils.h 
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_setup.o:        	mc_setup.cpp mc_setup.h mc_confg.h mc_utils.h 
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
-mc_randg.o:        	mc_randg.cc mc_randg.h mc_input.h mc_confg.h mc_utils.h  
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_randg.o:        	mc_randg.cpp mc_randg.h mc_input.h mc_confg.h mc_utils.h  
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
-mc_utils.o:        	mc_utils.cc mc_utils.h mc_confg.h 
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_utils.o:        	mc_utils.cpp mc_utils.h mc_confg.h 
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
-mc_poten.o:        	mc_poten.cc mc_poten.h mc_setup.h mc_confg.h mc_utils.h mc_input.cc
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_poten.o:        	mc_poten.cpp mc_poten.h mc_setup.h mc_confg.h mc_utils.h mc_input.cpp
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
-mc_clsmc.o:        	mc_clsmc.cc mc_clsmc.h mc_poten.h mc_setup.h mc_utils.h mc_estim.h
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_clsmc.o:        	mc_clsmc.cpp mc_clsmc.h mc_poten.h mc_setup.h mc_utils.h mc_estim.h
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
-mc_estim.o:        	mc_estim.cc mc_estim.h mc_setup.h mc_utils.h mc_poten.h mc_input.h mc_confg.h 
-			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cc
+mc_estim.o:        	mc_estim.cpp mc_estim.h mc_setup.h mc_utils.h mc_poten.h mc_input.h mc_confg.h 
+			$(CC) $(options) -c  $(CFLAGS) -o $@ $*.cpp
 
 .PHONY: clean
 			
---
+++ mc_input.cpp
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#include "mc_confg.h"
+#include "mc_input.h"
+#include "mc_utils.h"
+#include "mc_poten.h"
+#include "mc_setup.h"
+
+const char IO_OUTPUTDIR[]      = "OUTPUTDIR";
+const char IO_FILENAMEPREFIX[] = "FILENAMEPREFIX";
+const char IO_TEMPERATURE[]    = "TEMPERATURE";
+const char IO_ATOM[]           = "ATOM";
+const char IO_NUMBEROFSTEPS[]  = "NUMBEROFSTEPS";
+const char IO_NUMBEROFBLOCKS[] = "NUMBEROFBLOCKS";
+const char IO_MCSKIP_RATIO[]   = "MCSKIP_RATIO";
+const char IO_MCSKIP_TOTAL[]   = "MCSKIP_TOTAL";
+const char IO_MCSKIP_AVERG[]   = "MCSKIP_AVERG";
+
+string OutputDir;
+string FNPrefix;
+
+string MCFileName;     // mc output file name (no extension)
+
+void IOReadParams(const char in_file[])
+{
+   const char *_proc_=__func__;    // "MCReadInput()"; 
+
+   NDIM=3;
+   string params;
+   ifstream inf(in_file,ios::in);
+   if (!inf.good())
+   _io_error(_proc_,IO_ERR_FOPEN,in_file);
+   while (inf>>params)
+   {
+     if (params==IO_OUTPUTDIR)
+     {
+        inf>>OutputDir;
+     }
+     else
+     if (params==IO_FILENAMEPREFIX)
+     {
+        inf>>FNPrefix;
+     }
+     else
+     if (params==IO_TEMPERATURE)
+     {
+        inf>>Temperature;
+     }
+     else
+     if (params==IO_ATOM)
+     {
+         inf>>MCAtom.type;     //[1]
+         inf>>MCAtom.mcstep;   //[2]
+         inf>>MCAtom.fpot;     //[3]
+         inf>>MCAtom.fpos;     //[4]        
+     }
+     else
+     if (params==IO_NUMBEROFBLOCKS)
+     {
+         inf>>NumberOfMCBlocks>>NumberOfEQBlocks;
+     }
+     else
+     if (params==IO_NUMBEROFSTEPS)
+     {
+         inf>>NumberOfMCSteps;
+     }
+     else
+     if (params==IO_MCSKIP_RATIO)
+     {
+        inf >> MCSKIP_RATIO;
+     }
+     else
+     if (params==IO_MCSKIP_TOTAL)
+     {
+        inf >> MCSKIP_TOTAL;
+     }
+     else
+     if (params==IO_MCSKIP_AVERG)
+     {
+        inf >> MCSKIP_AVERG;
+     }
+     else
+     {}
+     getline(inf,params,'\n');  // skip comments at the end of the line 
+   }
+   inf.close();
+   MCFileName = (OutputDir+FNPrefix);
+// begin DUMP
+   cout << "OutPut   "          <<OutputDir<<endl;
+   cout << "File Name Prefix   "<<FNPrefix<<endl;
+   cout << "Temperature "<<Temperature<<endl;
+   int w=6;
+   cout << setw(w) << MCAtom.type << BLANK;
+   cout << setw(w) << MCAtom.mcstep << BLANK;
+   cout << setw(w) << MCAtom.fpot   << BLANK;
+   cout << setw(w) << MCAtom.fpos   << BLANK;
+
+   cout << endl;
+   cout << "Number of Steps in block = " << NumberOfMCSteps << endl;
+   cout << "Number of Blocks = " << NumberOfMCBlocks << BLANK << NumberOfEQBlocks << endl;
+
+   cout << endl;
+
+   cout << "Number of steps to skip to save ACCEPT RATIO" << BLANK << MCSKIP_RATIO << endl;
+   cout << "Number of steps to skip to save ACCUML AVERG" << BLANK << MCSKIP_TOTAL << endl;
+   cout << "Number of steps to skip to evaluate AVERAGES" << BLANK << MCSKIP_AVERG << endl;
+
+}
+
+int FileExist (const char fileName [])
+{
+   FILE *infile = fopen (fileName, "r");
+// ifstream  infile(fileName,ios::in); 
+
+   int ret_code=0;
+   if (infile == NULL)
+       ret_code = 0;    // file doesn't exist
+   else
+   {
+       ret_code = 1;     // file exists
+       fclose (infile);
+   }
+   return (ret_code);
+}
+
+void io_setout(fstream &out, int set_precision)
+{
+   out<<setprecision(set_precision);
+   out<<setiosflags (ios::scientific);
+// out<<setiosflags (ios::right);
+}
+
+void IOFileBackUp(const char file_name[])
+{
+#ifdef DEBUG_PIMC
+   const char *_proc_=__func__;
+#endif
+
+   string cmd="cp";
+
+   if (FileExist(file_name))
+   {
+      cmd += " " + (string)file_name;
+      cmd += " " + (string)file_name+(string)FBACKUP_EXTENSION;
+      int status = system(cmd.c_str());
+
+#ifdef DEBUG_PIMC
+      if (status < 0)
+      nrerror(_proc_,IO_ERR_SCALL);
+#endif
+   }
+}
+
+             
